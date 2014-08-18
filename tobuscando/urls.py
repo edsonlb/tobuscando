fa1d30e26706ef3f0c6ce1956f7bccba2fd7a8b4
matheus <oliveira.matheusde@gmail.com>
2014-08-18 20:41:35 -0300

    add static url

--- tobuscando/urls.py
+++ tobuscando/urls.py
@@ -1,4 +1,7 @@
-from django.conf.urls import patterns, include, url
+# coding: utf-8
+from django.conf.urls import patterns, include, url, static
+from django.conf.urls.static import static
+from django.conf import settings
 
 from django.contrib import admin
 admin.autodiscover()
@@ -16,3 +19,5 @@ urlpatterns = patterns('',
     url(r'^', include('tobuscando.ads.urls', namespace='ads')),
     url(r'^', include('tobuscando.core.urls', namespace='core')),
 )
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
