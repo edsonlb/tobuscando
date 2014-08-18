f4e4f5e2b4bfe0757113e4a12f06f69416f3e89e
matheus <oliveira.matheusde@gmail.com>
2014-08-18 20:20:29 -0300

    settings dj-decouple

--- tobuscando/settings.py
+++ tobuscando/settings.py
@@ -121,7 +121,7 @@ STATIC_URL = '/static/'
 
 CLOUDINARY = {
     'cloud_name': config('CLOUDINARY_NAME'),
-    'api_key': config('CLOUDINARY_API_KEY', cast=int),
+    'api_key': config('CLOUDINARY_API_KEY'),
     'api_secret': config('CLOUDINARY_API_SECRET')
 }
 
