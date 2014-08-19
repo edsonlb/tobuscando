1cab8728fd9479d05b600419f379f2239be6fa44
matheus <oliveira.matheusde@gmail.com>
2014-08-18 22:03:14 -0300

    Ad form

--- tobuscando/ads/forms.py
+++ tobuscando/ads/forms.py
@@ -38,27 +38,27 @@ class CategoryMetaFormInline(forms.ModelForm):
         self.fields['meta'].label = self.instance.meta.name
         self.fields['meta'].widget.attrs['class'] = 'hide'
 
-        self.fields['options'].label = ''
-
         if self.instance.meta.field in ['text', 'textarea']:
-            self.fields['options'] = forms.CharField(required=False)
+            self.fields['options'] = forms.CharField(label='', required=False)
         else:
             self.fields['options'] = forms.ModelChoiceField(
-                required=False, queryset=option_list)
+                label='', required=False, queryset=option_list)
 
         if self.instance.meta.field == 'textarea':
-            self.fields['options'].widget = forms.TextareaInput()
+            self.fields['options'].widget = forms.TextareaInput(label='')
 
         if self.instance.meta.field == 'checkbox':
-            self.fields['options'].widget = forms.CheckboxSelectMultiple()
+            self.fields[
+                'options'].widget = forms.CheckboxSelectMultiple(label='')
 
         if self.instance.meta.field == 'radio':
-            self.fields['options'].widget = forms.RadioSelectMultiple()
+            self.fields['options'].widget = forms.RadioSelectMultiple(label='')
 
         if self.instance.meta.field == 'date':
             self.fields['options'] = forms.DateField(label='', required=False)
         if self.instance.meta.field == 'datetime':
-            self.fields['options'] = forms.DateTimeField(label='', required=False)
+            self.fields['options'] = forms.DateTimeField(
+                label='', required=False)
 
     class Meta:
         model = CategoryMeta
