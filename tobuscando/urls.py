3cfa9536568ad663764a2c2352f0c99780d7751c
alexfalcucci <alex.falcucci@hotmail.com>
2014-08-19 01:46:22 -0300

    login success

--- tobuscando/urls.py
+++ tobuscando/urls.py
@@ -10,6 +10,9 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
 
     url(r'^$', 'tobuscando.core.views.home', name='home'),
+
+    #urls allauth
+    (r'^accounts/', include('allauth.urls')),
     #url(r'^login/$', 'tobuscando.core.views.login', name='login'),
     #url(r'^login/$', 'tobuscando.core.views.login', name='login'),
 
