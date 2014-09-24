24e618dad203882d3ae3af022d8e51bf67037367
matheus <oliveira.matheusde@gmail.com>
2014-09-23 23:07:12 -0300

    core/ad/views/news

--- tobuscando/ads/views.py
+++ tobuscando/ads/views.py
@@ -1,4 +1,5 @@
 # coding: utf-8
+
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.core.urlresolvers import reverse as r
