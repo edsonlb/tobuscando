d5a32fa17afbbf97922f534080e2312d0e079f88
matheus <oliveira.matheusde@gmail.com>
2014-09-27 23:13:45 -0300

    offer min price

--- tobuscando/ads/forms.py
+++ tobuscando/ads/forms.py
@@ -42,9 +42,10 @@ class OfferForm(forms.ModelForm):
             'link': forms.URLInput(attrs={
                 'placeholder': 'http://tobuscando/seuproduto'
             }),
+            'price': forms.TextInput(),
             'ad': forms.HiddenInput(),
             'person': forms.HiddenInput(),
-            'parent': forms.HiddenInput()
+            'parent': forms.HiddenInput(),
         }
 
 
