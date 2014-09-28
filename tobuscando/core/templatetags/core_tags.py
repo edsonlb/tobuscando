498c5e350cff3141f98bc00c160980403dbccd61
matheus <oliveira.matheusde@gmail.com>
2014-09-27 22:21:43 -0300

    truncatechars html

--- tobuscando/core/templatetags/core_tags.py
+++ tobuscando/core/templatetags/core_tags.py
@@ -19,20 +19,3 @@ def brl(value):
         return preco
     except:
         return ''
-
-
-@register.filter(is_safe=True)
-@stringfilter
-def truncatechars_html(value, arg):
-    """
-    Truncates HTML after a certain number of chars.
-
-    Argument: Number of chars to truncate after.
-
-    Newlines in the HTML are preserved.
-    """
-    try:
-        length = int(arg)
-    except ValueError:  # invalid literal for int()
-        return value  # Fail silently.
-    return Truncator(value).chars(length, html=True)
