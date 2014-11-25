dec8b1379848959d1698322e0895b34e996adc04
edsonlb <ti_edson@yahoo.com.br>
2014-11-25 21:12:53 -0200

    Ajustando Dash com nova area

--- tobuscando/core/models.py
+++ tobuscando/core/models.py
@@ -75,10 +75,10 @@ class Person(AbstractUser):
         return self.offer_set.filter(ad__person=self.pk)
 
     def offers_give(self):
-        return self.offer_set.filter(person__id=self.pk)
+        return self.offer_set.all().filter(person__id=self.pk)
 
-    def offers_receive(self):
-        return self.offer_set.filter(ad__person__id=self.pk, person__id__gte=0)
+    def offers_receive(self): #filtro está buscando.
+        return self.offer_set.all().filter(ad__person__id=self.pk)
 
     def get_accounts_facebook(self):
         account_fc = SocialAccount.objects.filter(user_id=self.id)
