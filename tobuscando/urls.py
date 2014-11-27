ac5f36494571cf35ec40ac5a5f0e1515f26d0be0
edsonlb <ti_edson@yahoo.com.br>
2014-11-27 20:13:34 -0200

    Ajustando login por cadastro

--- tobuscando/urls.py
+++ tobuscando/urls.py
@@ -7,9 +7,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'tobuscando.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/', include('allauth.urls')),
     url(r'^dashboard/', include('tobuscando.dashboard.urls', namespace='dashboard')),
