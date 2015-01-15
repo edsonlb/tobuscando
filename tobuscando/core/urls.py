f5c2510462a340e91cdac6aedc6e06e2732364b4
alexfalcucc <alex.falcucci@gmail.com>
2015-01-15 08:18:27 -0200

    PEP-8 urls

--- tobuscando/core/urls.py
+++ tobuscando/core/urls.py
@@ -5,10 +5,12 @@ from .views import HomeView, SearchView, PersonAdView
 
 
 urlpatterns = patterns('tobuscando.core.views',
-	url(r'busca/$', SearchView.as_view(), name='search'),
-    url(r'busca/(?P<slug>[\w_-]+)/$', SearchView.as_view(), name='search'),
-    url(r'pages/(?P<url>.*/)$', flatpage, name='flatpage'),
-    url(r'contato/$', 'contact', name='contact'),
-    url(r'person/(?P<username>[\w_-]+)/$', PersonAdView.as_view(), name="person_view"),
-    url(r'^$', HomeView.as_view(), name='home'),
-)
+                       url(r'busca/$', SearchView.as_view(), name='search'),
+                       url(r'busca/(?P<slug>[\w_-]+)/$',
+                           SearchView.as_view(), name='search'),
+                       url(r'pages/(?P<url>.*/)$', flatpage, name='flatpage'),
+                       url(r'contato/$', 'contact', name='contact'),
+                       url(r'person/(?P<username>[\w_-]+)/$',
+                           PersonAdView.as_view(), name="person_view"),
+                       url(r'^$', HomeView.as_view(), name='home'),
+                       )
