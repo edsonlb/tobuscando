be7eb8628b6923f08c926d09fd7dafc1993f79e8
edsonlb <ti_edson@yahoo.com.br>
2015-01-14 23:29:31 -0200

    Tentando resolver o problema do login

--- tobuscando/urls.py
+++ tobuscando/urls.py
@@ -14,4 +14,4 @@ urlpatterns = patterns('',
     url(r'^', include('tobuscando.core.urls', namespace='core')),
 )
 
-#urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
