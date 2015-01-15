7f9c383ea2d38133c970169f6c0534a9a49512fc
edsonlb <ti_edson@yahoo.com.br>
2015-01-14 23:26:32 -0200

    Tentando resolver o problema do login

--- tobuscando/core/urls.py
+++ tobuscando/core/urls.py
@@ -5,10 +5,8 @@ from .views import HomeView, SearchView, PersonAdView
 
 
 urlpatterns = patterns('tobuscando.core.views',
-	url(r'busca/$',
-        SearchView.as_view(), name='search'),
-    url(r'busca/(?P<slug>[\w_-]+)/$',
-        SearchView.as_view(), name='search'),
+	url(r'busca/$', SearchView.as_view(), name='search'),
+    url(r'busca/(?P<slug>[\w_-]+)/$', SearchView.as_view(), name='search'),
     url(r'pages/(?P<url>.*/)$', flatpage, name='flatpage'),
     url(r'contato/$', 'contact', name='contact'),
     url(r'person/(?P<username>[\w_-]+)/$', PersonAdView.as_view(), name="person_view"),
